8931c0bf2371720f231b3601105495599041f917
Stefan Hoffmann <stho32github@gmail.com>
2020-07-17 20:24:05 +0200

    add powershell scripts for building and watching

---
+++ watch.ps1
@@ -0,0 +1,7 @@
+<#
+    This file is a micro build process for the project.
+    You should have `choco install sass` on your windows computer
+    and hopefully the correct version as documented in the README.md
+#>
+
+sass source/scss/styles.scss Source/css/styles.css --style compressed --watch
