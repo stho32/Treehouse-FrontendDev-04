47db3dc621d5c6c0359e4cef64cc28f6dffe920b
Stefan Hoffmann <stho32github@gmail.com>
2020-07-18 09:19:44 +0200

    add $PSScriptRoot so execution of the scripts does not have to be started in the root of the repository but can be from anywhere

--- watch.ps1
+++ watch.ps1
@@ -3,5 +3,6 @@
     You should have `choco install sass` on your windows computer
     and hopefully the correct version as documented in the README.md
 #>
+Set-Location $PSScriptRoot
 
 sass source/scss/styles.scss Source/css/styles.css --style compressed --watch
